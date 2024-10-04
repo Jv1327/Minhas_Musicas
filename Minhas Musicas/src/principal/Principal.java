33b422cd773ca82dae9874e3242354a497c40de1
João Victor Rodrigues de Oliveira <jvrodrigues132770@gmail.com>
2024-10-04 12:18:38 -0300

    Add files via upload

---
+++ Minhas Musicas/src/principal/Principal.java
@@ -0,0 +1,37 @@
+package principal;
+
+import minhasmusicas.modelos.MinhasPreferidas;
+import minhasmusicas.modelos.Musica;
+import minhasmusicas.modelos.Podcast;
+
+public class Principal {
+    public static void main(String[] args) {
+        Musica minhaMusica = new Musica();
+        minhaMusica.setTitulo("The Motto");
+        minhaMusica.setCantor("Theodor");
+
+        for (int i = 0; i < 1000; i++) {
+            minhaMusica.reproduz();
+        }
+
+        for (int i = 0; i < 50; i++) {
+            minhaMusica.curte();
+        }
+
+        Podcast meuPodcast = new Podcast();
+        meuPodcast.setTitulo("F1rst Cast");
+        meuPodcast.setApresentador("F1rst");
+
+        for (int i = 0; i < 5000; i++) {
+            meuPodcast.reproduz();
+        }
+
+        for (int i = 0; i < 1000; i++) {
+            meuPodcast.curte();
+        }
+
+        MinhasPreferidas preferidas = new MinhasPreferidas();
+        preferidas.inclui(meuPodcast);
+        preferidas.inclui(minhaMusica);
+    }
+}
